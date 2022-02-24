8d63a35ad7a6ed45c748fcac644b20d0ae6fccb6
binitha.s <binitha.s@experionglobal.com>
2022-02-24 14:54:34 +0530

    adds repos and controller

---
+++ Clinic Management System/Repository/ILabTechnicianRepository.cs
@@ -0,0 +1,37 @@
+﻿using CMSFullProject.Models;
+using CMSFullProject.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSFullProject.Repository
+{
+    public interface ILabTechnicianRepository
+    {
+        //get patient by id 
+        Task<TestReportViewModel> GetPatientDetails(int? id);
+    
+        //get test details using advice id
+        Task<List<TestAmountViewModel>> GetTestDetails(int? id);
+
+        //get patient by id 
+        Task<List<TestReportViewModel>>GetPatient(int? id);
+
+        //add lab bill
+        Task<int> AddBill(LabBills labbill);
+
+
+        //create report id
+        Task<int> CreateReport(TestReportNew report);
+
+        //insert labreport
+        Task<int> LabReport(Reportlist report);
+
+        Task<Authentications> GetUserByNameandPassword(string name, string password);
+
+
+
+
+    }
+}
